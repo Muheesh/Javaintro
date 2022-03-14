88919008d82c17c8dba65dff6647ad6a2994a0ad
Muheesh <muheeshas@gmail.com>
2022-03-14 12:23:17 +0530

    ifelse

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,9 +1,13 @@
 public class Helloworld {
 
     public static void main(String[] args) {
-        Integer a=10,b=15,c;
-        c=a+b;
-        System.out.println(c);
+        Integer a=10;
+        if(a<20){
+        System.out.println("It is less than 20");
 
+    }
+        else{
+            System.out.println("it is greater than 20");
+        }
     }
 }
